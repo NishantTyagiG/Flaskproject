f7b826f739760d96578076fb15d4e2b6b4d02ea8
Tyagi <ntyagi@dxc.com>
2025-11-21 14:37:43 +0530

    feat: add database check in /ready endpoint

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ app=Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+app.route("/health")
 def liveness_probe():
-   
-    return {'status': 'alive'}, 200
+     return {'status': 'alive'}, 200
+
+
 app.run(host="0.0.0.0",port=3000)
 
 
